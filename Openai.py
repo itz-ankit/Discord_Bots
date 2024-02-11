c88073636e5898f09222374b7a32a2abaf2e37fb
XIAO <116746318+itz-ankit@users.noreply.github.com>
2024-02-11 12:12:17 +0530

    Update Openai.py

--- Openai.py
+++ Openai.py
@@ -1,7 +1,7 @@
 import os
 import openai
 
-openai.api_key = os.getenv("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")
+openai.api_key = os.getenv("")
 
 response = openai.Completion.create(
   model="text-davinci-003",
